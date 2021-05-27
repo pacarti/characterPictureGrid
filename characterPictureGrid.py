289d720f89ae713879d430342eced262b9f87403
pacarti <59978338+pacarti@users.noreply.github.com>
2021-05-27 11:01:30 +0200

    Update characterPictureGrid.py
    Determines the height of picture automatically.

--- characterPictureGrid.py
+++ characterPictureGrid.py
@@ -8,8 +8,9 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
 
+gridHeight = len(grid[0])
 
-for x in range(6):
+for x in range(gridHeight):
     print('')
     for y in range(9):
         print(grid[y][x], end = '')
